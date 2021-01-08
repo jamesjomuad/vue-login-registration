ee556c230529d95185a81e27fa271b8c38d9c8e8
Unknown <jomuad_james@yahoo.com>
2021-01-08 16:31:46 +0800

    cleaning

--- src/views/Dashboard.vue
+++ src/views/Dashboard.vue
@@ -2,17 +2,6 @@
     <div>
         <Sidebar/>
 
-        <v-app-bar app>
-            <v-toolbar-title>
-                <v-icon>mdi-electron-framework</v-icon> &nbsp; {{pageTitle}}
-            </v-toolbar-title>
-            <v-spacer></v-spacer>
-            <v-btn icon>
-                <v-icon>mdi-refresh</v-icon>
-            </v-btn>
-            <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
-        </v-app-bar>
-
         <!-- Sizes your content based upon application components -->
 
             <!-- Provides the application the proper gutter -->
@@ -32,10 +21,5 @@ export default {
     components: {
         Sidebar: () => import('../components/Sidebar')
     },
-    data(){
-        return {
-            pageTitle: "Dashboard"
-        }
-    }
 }
 </script>
